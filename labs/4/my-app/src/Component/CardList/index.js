6f4e67689445a5280cb19a51d220f9bcea7d098a
Matthew Mckenzie <mojo@matthews-air.home>
2020-02-26 21:03:07 -0500

    complete

---
+++ labs/4/my-app/src/Component/CardList/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import todoList from './todoList.json'
+import Card from '../Card'
+
+class CardList extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={ 
+            cards:todoList.cards.map((v,i) => {
+            v.id = i
+            return v
+            })
+        }
+    }
+    remCard(id){
+        let cards = this.state.cards.filter((v) => v.id !== id)
+        this.setState({cards:cards})
+    }
+    render(){
+        return (
+            this.state.cards.map((v,i) => 
+            <Card key = {v.id} remCard = {() => this.remCard(v.id)} title ={v.title} content = {v.content} />
+        ))
+    }
+}
+
+export default CardList;
