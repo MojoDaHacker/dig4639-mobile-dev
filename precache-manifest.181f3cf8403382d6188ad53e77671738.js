e5afda851c514ad46b1bf1d4cbca1ebc3c458064
Matthew Mckenzie <mojo@Matthews-MacBook-Air.local>
2020-03-04 12:24:21 -0500

    Updates

---
+++ precache-manifest.181f3cf8403382d6188ad53e77671738.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cd6bcf3b5362debcba08fb8c8d8f09b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dd1b13f568ade9e254c",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6b46b337fa14a55f8643",
+    "url": "/static/js/2.f493ac8a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.f493ac8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dd1b13f568ade9e254c",
+    "url": "/static/js/main.855efe8b.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  }
+]);
