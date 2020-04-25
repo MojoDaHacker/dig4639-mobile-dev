70bbcadaa8c26ff569abc0138053932e37491ce2
Matthew McKenzie <mojo@matthews-air.home>
2020-04-24 23:39:38 -0400

    Contact App Project

--- project/Contacts_App/App.js
+++ project/Contacts_App/App.js
@@ -9,19 +9,40 @@ export default class App extends React.Component{
     super(props)
     this.state = {
       contacts : [],
-      showContactAdd: false
+      showContactAdd: false,
+      currProfile: "", currLength: 0
     }
   }
+  // Need to share the server -- 19006 I shared it
+  // Looking now
+  // Looks like making two fetch requests, 
+  // one before the first returned, was causing the error
+  // So just make sure you make the second request after the first completes
+
+  // Then make sure you change the state (using setState)
+
+//everytime I add a contact i have to hard refresh
+
   componentDidMount() {
-    fetch("http://plato.mrl.ai:8080/contacts", {headers: {API: "mckenzie"}})
+    fetch("http://plato.mrl.ai:8080/contacts",
+    {method: "GET", 
+    headers: {API: "mckenzie", "Content-Type": "application/json"}
+    })
     .then((res) => res.json())
     .then((data) => {
       this.setState({contacts: data.contacts});
     });
+    fetch("http://plato.mrl.ai:8080/profile",
+    {method: "GET", 
+    headers: {API: "mckenzie", "Content-Type": "application/json"}
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      this.setState({currProfile: data.name, currLength: data.count});
+    });
   }
   addContacts(){
     this.setState({showContactAdd:true})
-    
   }
   hideAddContact() {
     this.setState({showContactAdd:false})
@@ -30,27 +51,47 @@ export default class App extends React.Component{
   showContacts(){
     return (
       this.state.contacts.map((value, index) => {
-        return <Card key={index} name= {value.name}/>
+        return <Card key={index} name= {value.name} remContact={() => this.remContact(index)} />
       })
     )
   }
   receiveData(data) {
     console.log(data);
     console.log(JSON.stringify({name: data.name, number: data.number}))
-    fetch("http://plato.mrl.ai:8080/contacts/add", 
+    fetch("http://plato.mrl.ai:8080/contacts/add",
     {method: "POST", 
-    headers: {API: "mckenzie"},
+    headers: {API: "mckenzie", "Content-Type": "application/json"},
     body: JSON.stringify({name: data.name, number: data.number})
+  })
+  .then((res) => res.json())
+  .then((data) => {
+      let tempArray = [...this.state.contacts]
+      tempArray.push(data.added)
+      this.setState({contacts: tempArray, currLength: this.state.currLength + 1})
+    })
+    this.hideAddContact();
+  }
+  remContact(position){
+    console.log(position);
+    fetch("http://plato.mrl.ai:8080/contacts/remove", 
+    {method: "POST", 
+    headers: {API: "mckenzie", "Content-Type": "application/json"},
+    body: JSON.stringify({"position": position })
     })
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => console.log(data))
+    // this.updateContacts();
+    
+    let tempArray = [...this.state.contacts];
+    tempArray.splice(position,1)
+    this.setState({contacts : tempArray,  currLength: this.state.currLength - 1})
   }
 
   render(){
     let Screen;
     if(this.state.showContactAdd) {
       Screen = () => <>
-        <Text>Current Contacts</Text>
+        <Text>{this.state.currProfile}'s {this.state.currLength} Contacts</Text>
         <Text>{this.showContacts()}</Text>
         <Icon name="ios-add" type="ionicon" onPress={() => this.addContacts()}/>
         <AddData 
@@ -59,7 +100,7 @@ export default class App extends React.Component{
       </>
     } else {
       Screen = () => <>
-        <Text>Current Contacts</Text>
+        <Text>{this.state.currProfile}'s {this.state.currLength} Contacts</Text>
         <Text>{this.showContacts()}</Text>
         <Icon name="ios-add" type="ionicon" onPress={() => this.addContacts()}/>
       </>
--- project/Contacts_App/components/card/index.js
+++ project/Contacts_App/components/card/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Icon } from 'react-native-elements';
 import './index.css';
 
 class Card extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {contacts : 0}
+    }
     render(){
         return (
-            <div className = 'card'>
-                <div>{this.props.name}</div>
-            </div>
+            <View>
+                 <div className = 'card'>
+                    <div >{this.props.name}</div>
+                    <Icon name="ios-close" type="ionicon" onPress={() => this.props.remContact()}/>
+                </div>
+            </View>
         )
     }
 }
